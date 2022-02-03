d2d9f6fb6b20ccd9aaeaf5d2cc72558c667457fd
zuoyecihua <zhiyumengxiang@outlook.com>
2022-02-03 16:37:57 +0800

    update .gitignore

--- utils/common_utils.py
+++
@@ -1,5 +0,0 @@
-import time
-
-def current_timestamp():
-    localtime = time.asctime( time.localtime(time.time()))
-    return time.strftime("%Y%m%d%H%M%S", time.localtime())
--- utils/random_util.py
+++
@@ -1,8 +0,0 @@
-import torch
-import numpy as np
-import random
-def set_random_seed(seed):
-    torch.manual_seed(seed)
-    np.random.seed(seed)
-    random.seed(seed)
-    torch.backends.cudnn.deterministic = True
