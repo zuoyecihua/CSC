e8dfe01ade3f74b109da0097b32f468a1cd3ead0
zuoyecihua <98077781+zuoyecihua@users.noreply.github.com>
2022-08-21 11:10:13 +0800

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -4,7 +4,7 @@ from sacred.observers import MongoObserver
 ex = Experiment('TestCSC')
 config_yaml = "test_error_judge.yaml"
 # ex.add_source_file("./configs/%s" %config_yaml)
-observer_mongo = MongoObserver.create(url='mongodb://admin:123456@114.212.86.198:27017/?authMechanism=SCRAM-SHA-1', db_name='db')
+observer_mongo = MongoObserver.create(url='', db_name='db')
 ex.observers.append(observer_mongo)
 
 #加载训练参数
